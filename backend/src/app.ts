a57baab542ed529d2154bb6888efa92466cc6be8
Nick <nicholas.newman1@hotmail.com>
2024-09-21 09:28:07 -0400

    add scraping endpoint with auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,8 @@ import menuItemsRoutes from "./routes/menuItemsRoutes";
 import restaurantsRoutes from "./routes/restaurantsRoutes";
 import { createResponse, ValidationError } from "./utils/responseUtil";
 import errorHandler from "./middlewares/errorHandler";
-// import scrapeMcDonaldsMenu from "./services/scrapeMcDonaldsMenu";
-// import scrapeWendysMenu from "./services/scrapeWendysMenu";
+import scrapeMcDonaldsMenu from "./services/scrapeMcDonaldsMenu";
+import scrapeWendysMenu from "./services/scrapeWendysMenu";
 
 const app = express();
 
@@ -23,11 +23,17 @@ app.use((_req: Request, res: Response, next: NextFunction) => {
 app.use(express.json());
 app.use("/api/menu-items", menuItemsRoutes);
 app.use("/api/restaurants", restaurantsRoutes);
-// app.use("/scrape", (_, res) => {
-//   scrapeWendysMenu();
-//   scrapeMcDonaldsMenu();
-//   res.send("Scraping menus...");
-// });
+app.use("/scrape", (req, res) => {
+  const apiKey = req.headers["authorization"];
+  const validApiKey = process.env.ADMIN_API_KEY;
+  if (apiKey === `Bearer ${validApiKey}`) {
+    scrapeWendysMenu();
+    scrapeMcDonaldsMenu();
+    res.send("Scraping menus...");
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+});
 
 app.use(errorHandler);
 
