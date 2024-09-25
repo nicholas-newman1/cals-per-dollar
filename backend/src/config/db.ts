cf27588d67ee45dd823a5f34c9d862980c261a7a
Nicholas Newman <62115019+nicholas-newman1@users.noreply.github.com>
2024-09-25 15:02:55 -0400

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,8 +6,12 @@ dotenv.config();
 const dbUrl = process.env.CLEARDB_DATABASE_URL;
 
 const db = dbUrl
-  ? mysql.createConnection(dbUrl)
-  : mysql.createConnection({
+  ? mysql.createPool({
+      uri: dbUrl,
+      keepAliveInitialDelay: 10000,
+      enableKeepAlive: true
+    })
+  : mysql.createPool({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
@@ -17,12 +21,14 @@ const db = dbUrl
       enableKeepAlive: true
     });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("Error connecting to the database:", err);
     process.exit(1);
   }
   console.log("Connected to the database");
+
+  if (connection) connection.release();
 });
 
 export default db;
