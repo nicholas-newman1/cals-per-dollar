c14e00eebd63c95d89d458f4f3f85a5d1b0c413e
Nick <nicholas.newman1@hotmail.com>
2024-10-27 07:26:06 -0400

    call mysqldump directly

--- backend/src/scripts/databaseBackup.ts
+++ backend/src/scripts/databaseBackup.ts
@@ -1,5 +1,5 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { exec } from "child_process";
+import mysqldump from "mysqldump";
 import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
@@ -17,57 +17,55 @@ const s3Client = new S3Client({
 const databaseBackup = async () => {
   const jawsdbUrl = process.env.JAWSDB_URL;
   if (!jawsdbUrl) return;
+
   const {
-    hostname: dbHost,
-    username: dbUser,
-    password: dbPassword,
-    pathname: dbName,
+    hostname: host,
+    username: user,
+    password: password,
+    pathname,
   } = new URL(jawsdbUrl);
+  const database = pathname.slice(1);
+
   const bucketName = process.env.S3_BUCKET_NAME;
   const fileName = `db_backup_${new Date()
     .toISOString()
     .replace(/[:.]/g, "-")}.sql`;
   const filePath = path.join(__dirname, fileName);
-  const mysqldumpCommand = `mysqldump --no-tablespaces --set-gtid-purged=OFF --single-transaction -h ${dbHost} -u ${dbUser} -p${dbPassword} ${dbName} > ${fileName}`;
 
-  return new Promise((resolve, reject) => {
-    exec(mysqldumpCommand, async (err) => {
-      if (err) {
-        console.error("mysqldump failed:", err);
-        reject(err);
-        return;
-      }
-      console.log("Database dumped successfully.");
+  try {
+    await mysqldump({
+      connection: { host, user, password, database },
+      dumpToFile: filePath,
+    });
+    console.log("Database dumped successfully.");
 
-      try {
-        const fileData = fs.readFileSync(filePath);
-        const uploadParams = {
-          Bucket: bucketName,
-          Key: fileName,
-          Body: fileData,
-        };
-        const uploadCommand = new PutObjectCommand(uploadParams);
-        const uploadResult = await s3Client.send(uploadCommand);
+    const fileData = fs.readFileSync(filePath);
+    const uploadParams = {
+      Bucket: bucketName,
+      Key: fileName,
+      Body: fileData,
+    };
+    const uploadCommand = new PutObjectCommand(uploadParams);
+    const uploadResult = await s3Client.send(uploadCommand);
 
-        console.log(
-          "Backup successfully uploaded to S3:",
-          `https://${bucketName}.s3.amazonaws.com/${fileName}`
-        );
+    console.log(
+      "Backup successfully uploaded to S3:",
+      `https://${bucketName}.s3.amazonaws.com/${fileName}`
+    );
 
-        fs.unlink(filePath, (unlinkErr) => {
-          if (unlinkErr) {
-            console.error("Failed to delete local backup file:", unlinkErr);
-          } else {
-            console.log("Local backup file deleted.");
-          }
-        });
-        resolve(uploadResult);
-      } catch (uploadError) {
-        console.error("Failed to upload to S3:", uploadError);
-        reject(uploadError);
+    fs.unlink(filePath, (unlinkErr) => {
+      if (unlinkErr) {
+        console.error("Failed to delete local backup file:", unlinkErr);
+      } else {
+        console.log("Local backup file deleted.");
       }
     });
-  });
+
+    return uploadResult;
+  } catch (error) {
+    console.error("Error during database backup process:", error);
+    throw error;
+  }
 };
 
 export default databaseBackup;
